cdc6df1686f6c5d2824bb0e5ab6e5d6dd567535a
hungdoan1031 <dth1vn@gmail.com>
2019-12-09 23:25:48 -0500

    Completed rosters page

--- src/app/animation.ts
+++ src/app/animation.ts
@@ -10,7 +10,7 @@ export const slideInAnimation =
           top: 0,
           left: 0,
           width: '100%',
-          //transition: 'all 1s linear',
+          transition: 'all',
           'transform-origin': 'right',
           'transform-style': 'preserve-3d',          
         })
@@ -18,26 +18,40 @@ export const slideInAnimation =
       query(':enter', [
         style({ 
             //left: '-100%'
-            transform: 'rotateY(180deg)',        
+            transform: 'rotateY(90deg)',  
+            'background-color': "white",
+            'box-shadow': '-50px -10px 50px black'      
+        })
+      ]),
+      query(':leave', [
+        style({ 
+          'z-index': '-1',    
         })
       ]),
       //query(':leave', animateChild()),
       group([
-        query(':leave', [
-          animate('500ms ease-in-out', 
-            style({ 
-                //transform: 'perspective(1000px) rotateY(-180deg)',
-                'z-index': '-1',
-            }))
-        ]),
+        // query(':leave', [
+        //   animate('10s ease-in-out', 
+        //     style({ 
+        //         //transform: 'perspective(1000px) rotateY(-180deg)',
+        //        // 'z-index': '-1',
+                
+        //     }))
+        // ]),
         query(':enter', [
           animate('500ms ease-in-out', 
             style({ 
                 //left: '0%'
-                transform: 'perspective(1000px) rotateY(0deg)',
+                transform: 'perspective(2000px) rotateY(0deg)',
+                'box-shadow': '-5px -1px 5px black'
+                //'background-color': "transparent",
             }))
         ])
       ]),
-      //query(':enter', animateChild()),
+      // query(':enter', [
+      //   style({   
+      //       'background-color': "transparent",     
+      //   })
+      // ]),
     ]),
   ]);
--- src/app/team-rosters/team-rosters.component.ts
+++ src/app/team-rosters/team-rosters.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { Team } from '../models/team';
 import { HttpService } from '../http.service';
+import { TeamMember } from '../models/teamMember';
 
 @Component({
   selector: 'app-team-rosters',
@@ -18,7 +19,31 @@ export class TeamRostersComponent implements OnInit {
   ngOnInit() {
     this.httpService.getTeams().subscribe(teams => {
       this.teams = teams;
-      console.log(teams);
+      // find the largest team member number
+      let largestNum = 0;
+      this.teams.forEach(function(team, index) {
+        if (largestNum < team.teamMembers.length) {
+          largestNum = team.teamMembers.length;
+        }
+      });
+
+      this.teams.forEach(function(team, index) {
+        if (team.teamMembers.length < largestNum) {
+          for (let i = 0; i < largestNum - team.teamMembers.length; i++) {
+            team.teamMembers.push({
+              id : '',
+              name: '',
+              email: '',
+              teamId :'',
+              shirtSize: null,
+              shirtSizeId:''              
+            })
+          }
+        }
+      });
+
+      console.log(this.teams);
+      // Look
     })
   }
 
